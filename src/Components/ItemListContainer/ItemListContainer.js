512098b07ec7ff2bd21f48358a8c10ec522f8d5b
Nicolas <56944680+nicoc12024@users.noreply.github.com>
2022-08-24 15:40:02 +0200

    m

--- src/Components/ItemListContainer/ItemListContainer.js
+++ src/Components/ItemListContainer/ItemListContainer.js
@@ -2,11 +2,8 @@ import "./ItemListContainer.css";
 import ItemList from "../ItemList/ItemList";
 import { useParams } from "react-router-dom";
 import { getProducts } from "../../services/firebase/firestore";
-<<<<<<< HEAD
 import NewsLetter from "./NewsLetter/NewsLetter";
-=======
 import { useAsync } from "../../hooks/useAsync";
->>>>>>> customHook
 
 function ItemListContainer() {
   const { categoryId } = useParams();
@@ -15,41 +12,17 @@ function ItemListContainer() {
 
   const { data, error, isLoading } = useAsync(getProductsFromFirestore, [categoryId]);
 
-  // const [products, setProducts] = useState([]);
-  // const [loading, setLoading] = useState(true);
-
-  // useEffect(() => {
-  //   setLoading(true);
-
-  //   getProducts(categoryId)
-  //     .then((products) => {
-  //       setProducts(products);
-  //     })
-  //     .catch((error) => {
-  //       console.log(error);
-  //     })
-  //     .finally(() => {
-  //       setLoading(false);
-  //     });
-  // }, [categoryId]);
-
   if (isLoading) {
     return <h1 className="spinner">Loading products...</h1>;
   }
 
   return (
-<<<<<<< HEAD
     <>
       <div className="itemListContainer">
-        <ItemList products={products} />
+        <ItemList products={data} />
       </div>
       <NewsLetter />
     </>
-=======
-    <div className="itemListContainer">
-      <ItemList products={data} />
-    </div>
->>>>>>> customHook
   );
 }
 export default ItemListContainer;
